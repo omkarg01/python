8df084666a10ad47913161fcb7df06ec53d5aa41
omkar01 <omkargujja01@gmail.com>
2020-12-02 16:31:04 +0530

    Django

---
+++ Django practise/instalog/insta_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ip330s\desktop\python_project\django practise\instalog\insta_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
