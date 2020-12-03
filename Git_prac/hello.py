0df97386388014d404cf8debadbc6d2f6f2ec2b3
omkar01 <omkargujja01@gmail.com>
2020-12-03 21:17:48 +0530

    pdf generate code

--- Git_prac/hello.py
+++ Git_prac/hello.py
@@ -1,8 +1,32 @@
-print('hello')
-print('world')
-print('added after creating branch')
-print('world branch')
-print('is baar branch')
-print('some changes')
-print('remove master added me')
+from selenium import webdriver
 
+def download_pdf(lnk):
+
+    from selenium import webdriver
+    from time import sleep
+
+    options = webdriver.ChromeOptions()
+
+    download_folder = "C:\\"
+
+    profile = {"plugins.plugins_list": [{"enabled": False,
+                                         "name": "Chrome PDF Viewer"}],
+               "download.default_directory": download_folder,
+               "download.extensions_to_open": ""}
+
+    options.add_experimental_option("prefs", profile)
+
+    print("Downloading file from link: {}".format(lnk))
+
+    driver = webdriver.Chrome(r'C:\Users\IP330s\Downloads\chromedriver_win32 (1)\chromedriver.exe', options=options)  # Optional argument, if not specified will search path.
+    driver.get(lnk)
+
+    filename = lnk.split("/")[4].split(".cfm")[0]
+    print("File: {}".format(filename))
+
+    print("Status: Download Complete.")
+    print("Folder: {}".format(download_folder))
+
+    driver.close()
+    
+download_pdf('http://esamskriti.com/essays/BG-CH-1.pdf')
